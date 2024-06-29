984ad5d89a6da290d745976ab8894fb4794c7f42
Sachintha.d <Sachintha.d@DESKTOP-C7NM31N>
2024-06-29 15:45:47 +0530

    Add project files.

---
+++ ASPNET6_MVC_CRUD/Controllers/Employee.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET6_MVC_CRUD.Controllers
+{
+    public class Employee : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ASPNET6_MVC_CRUD/Data/MVCDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ASPNET6_MVC_CRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNET6_MVC_CRUD.Data
+{
+    public class MVCDbContext : DbContext
+    {
+        public MVCDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
